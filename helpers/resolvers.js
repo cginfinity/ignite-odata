3c87134ca7eea4a43598de20076c501452688c82
Sharma <ravikant.sharma@cybergindia.local>
2020-06-02 20:21:48 +0530

    Created Odata Node and basic CRUD resolvers

---
+++ helpers/resolvers.js
@@ -0,0 +1,24 @@
+const { GetQuery: CassandraGetQuery } = require('./resolvers/cassandra');
+const { GetQuery: db2GetQuery } = require('./resolvers/db2');
+const { GetQuery: mssqlGetQuery } = require('./resolvers/mssql');
+const { GetQuery: mysqlGetQuery } = require('./resolvers/mysql');
+const { GetQuery: oracleGetQuery } = require('./resolvers/oracle');
+const { GetQuery: postgresqlGetQuery } = require('./resolvers/postgresql');
+
+exports.GetQueries = async (info) => {
+  try {
+    return queries = await {
+      cassandra: await CassandraGetQuery(info),
+      db2: await db2GetQuery(info),
+      mssql: await mssqlGetQuery(info),
+      mysql: await mysqlGetQuery(info),
+      oracle: await oracleGetQuery(info),
+      postgresql: await postgresqlGetQuery(info),
+    }
+  } catch (err) {
+    return (result = {
+      message: "Couldn't create query",
+      error: err.message
+    });
+  }
+};
---
+++ helpers/resolvers/oracle.js
@@ -0,0 +1,88 @@
+const { GetUpdateSetColumns, GetInsertionColumnsAndValues} = require('../sql')
+
+// returns a oracle db query base on url method and parameters
+exports.GetQuery = async (info) => {
+  try {
+    if (info.method === 'GET'){
+      return await this.GetSelectQuery(info)
+    }
+    else if (info.method === 'POST'){
+      return await this.GetInsertQuery(info)
+    }
+    else if (info.method === 'PUT'){
+      return await this.GetUpdateQuery(info)
+    }
+    else if (info.method === 'DELETE'){
+        return await this.GetDeleteQuery(info)
+    }
+  } catch (err) {
+    return (result = {
+      message: "Couldn't create oracle db query",
+      error: err
+    });
+  }
+};
+
+exports.GetSelectQuery = async (info) => {
+  try {
+    query = 'select * from tablename';
+    var resources = info.resource_path.split('/');
+    query = query.replace("tablename", resources[1]);
+    //ADD LOGIC TO ADD WHERE CONDITIONS
+
+
+      
+    return query
+  } catch (err) {
+    return (result = {
+      error: err
+    });
+  }
+};
+
+exports.GetInsertQuery = async (info) => {
+  try {
+    const data = GetInsertionColumnsAndValues(info.body);
+    query = `INSERT INTO tablename(${data.columns}) VALUES (${data.Values}) `
+    var resources = info.resource_path.split('/');
+    query = query.replace("tablename", resources[1]);
+    return query
+  } catch (err) {
+    return (result = {
+      error: err
+    });
+  }
+};
+
+exports.GetUpdateQuery = async (info) => {
+  try {
+    const setConditions = GetUpdateSetColumns(info.body);
+    query = `UPDATE tablename SET ${setConditions}`;
+    var resources = info.resource_path.split('/');
+    query = query.replace("tablename", resources[1]);
+    //ADD LOGIC TO ADD WHERE CONDITIONS
+
+
+    return query
+  } catch (err) {
+    return (result = {
+      error: err
+    });
+  }
+};
+
+exports.GetDeleteQuery = async (info) => {
+  try {
+    query = 'DELETE FROM tablename';
+    var resources = info.resource_path.split('/');
+    query = query.replace("tablename", resources[1]);
+    //ADD LOGIC TO ADD WHERE CONDITIONS
+
+
+    return query
+  } catch (err) {
+    return (result = {
+      error: err
+    });
+  }
+};
