9e29ad4e66f339cb48687e7c053a7820d8c9c5d7
Sharma <ravikant.sharma@cybergindia.local>
2020-06-05 17:38:38 +0530

    added operator resolver

---
+++ helpers/operators.js
@@ -0,0 +1,27 @@
+// returns the operator value based on odata expression in url
+const convertToOperator = (odataOperator) => {
+  let operator;
+  switch (odataOperator) {
+    case 'eq':
+      operator = '==';
+      break;
+    case 'ne':
+      operator = '!=';
+      break;
+    case 'gt':
+      operator = '>';
+      break;
+    case 'ge':
+      operator = '>=';
+      break;
+    case 'lt':
+      operator = '<';
+      break;
+    case 'le':
+      operator = '<=';
+      break;
+    default:
+      throw new Error('Invalid operator code, expected one of ["==", "!=", ">", ">=", "<", "<="].');
+  }
+  return operator;
+};
