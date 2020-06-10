9a9723bc117f5861ec626ea0d1cdf8e4d5d49e7d
Sharma <ravikant.sharma@cybergindia.local>
2020-06-10 19:30:14 +0530

    added resolvers for sqlite nad mariadb

--- helpers/resolvers.js
+++ helpers/resolvers.js
@@ -2,16 +2,20 @@ const { GetQuery: db2GetQuery } = require('./resolvers/db2');
 const { GetQuery: mssqlGetQuery } = require('./resolvers/mssql');
 const { GetQuery: mysqlGetQuery } = require('./resolvers/mysql');
 const { GetQuery: oracleGetQuery } = require('./resolvers/oracle');
-const { GetQuery: postgresqlGetQuery } = require('./resolvers/postgresql');
+const { GetQuery: postgresGetQuery } = require('./resolvers/postgres');
+const { GetQuery: mariadbGetQuery } = require('./resolvers/mariadb');
+const { GetQuery: sqliteGetQuery } = require('./resolvers/sqlite');
 
 exports.GetQueries = async (info) => {
   try {
     return queries = await {
       db2: await db2GetQuery(info),
+      mariadb: await mariadbGetQuery(info),
       mssql: await mssqlGetQuery(info),
       mysql: await mysqlGetQuery(info),
       oracle: await oracleGetQuery(info),
-      postgresql: await postgresqlGetQuery(info)
+      postgres: await postgresGetQuery(info),
+      sqlite: await sqliteGetQuery(info)
     }
   } catch (err) {
     return (result = {
