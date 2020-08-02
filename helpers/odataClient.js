c43e7ae43357f651b65ebb8ee6be2acaed81f918
RAVI KANT SHARMA <ravikantsharma2169@gmail.com>
2020-08-02 23:53:00 +0530

    Added and tested GET api calls

--- helpers/odataClient.js
+++ helpers/odataClient.js
@@ -1,37 +1,29 @@
 const { GetAsync, PostAsync, PutAsync, PatchAsync, DeleteAsync } = require('./callApi');
 const { GetQueryParamString } = require('./functions');
 
-exports.client = async (info) => {
+exports.client = async (clientInfo, queryParams) => {
   try {
-    console.log(info)
-    //
-    info.reqheaders ? headers = info.reqheaders : headers = {}
-    info.reqbody ? data = info.reqheaders : data = {}
-
-    ApiURL = info.rootURL + info.resource;
-    headers = info.reqheaders
-
-    queryParams = ''
-    info  ? console.log("true") : console.log("false")
-
-    //add logic to add request headers
-
-    //add logic to add request body
-
-    if (info.method === 'GET') {
-      return await GetAsync(ApiURL, headers)
+    url = clientInfo.rootURL + clientInfo.resource;
+    clientInfo.reqheaders ? headers = clientInfo.reqheaders : headers = {}
+    clientInfo.reqbody ? data = clientInfo.reqbody : data = {}
+    queryParamString = GetQueryParamString(queryParams)
+    if(queryParamString !== ''){
+      url = url + '?' + queryParamString
+    } 
+    if (clientInfo.method === 'GET') {
+      return await GetAsync(url, headers)
     }
-    else if (info.method === 'POST') {
-      return await PostAsync(ApiURL)
+    else if (clientInfo.method === 'POST') {
+      return await PostAsync(url, headers, data)
     }
-    else if (info.method === 'PUT') {
-      return await PutAsync(ApiURL)
+    else if (clientInfo.method === 'PUT') {
+      return await PutAsync(url, headers, data)
     }
-    else if (info.method === 'PATCH') {
-      return await PatchAsync(ApiURL)
+    else if (clientInfo.method === 'PATCH') {
+      return await PatchAsync(url, headers, data)
     }
-    else if (info.method === 'DELETE') {
-      return await DeleteAsync(ApiURL)
+    else if (clientInfo.method === 'DELETE') {
+      return await DeleteAsync(url, headers)
     }
     else {
       return "Couldn't process your request";
