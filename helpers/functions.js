a805fa4cfaa951f803f49d67c260694ae0b58b80
RAVI KANT SHARMA <ravikantsharma2169@gmail.com>
2020-08-02 23:53:35 +0530

    Added queries with paramters

--- helpers/functions.js
+++ helpers/functions.js
@@ -12,3 +12,22 @@ exports.getEntity = (entity) => {
     entity_with_param = entity.split('(');
     return entity_with_param[0];
 };
+
+// returns the columns and values for the insert query
+exports.GetQueryParamString = (data) => {
+    var queryString = '',
+      count = 0;
+    for (key in data) {
+      if (data[key] !== "" ) {
+        queryString =
+          count !== Object.keys(data).length - 1
+            ? queryString + key + "=" + data[key] + "&"
+            : queryString + key + "=" + data[key];
+        count++;
+      }
+    }
+    if (queryString.charAt(queryString.length - 1) === "&") {
+        queryString = queryString.substring(0, queryString.length - 1);
+    }
+    return queryString
+  };
